36ce5c4f6b7ff7e1d133a935eb7b6ac9681b07f2
sank2000 <santhoshvelr@yahoo.com>
2021-05-09 12:02:16 +0530

    added edit profile layout to student profile ✔

--- src/pages/Student/Profile.jsx
+++ src/pages/Student/Profile.jsx
@@ -1,31 +1,54 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
 import { rgba } from 'polished';
+import EditIcon from '@material-ui/icons/Edit';
+import { Fab } from '@material-ui/core';
 
 import { RowWithTypography } from 'components';
+import { adminLayout } from 'constants/classes';
+import Edit from './EditProfile';
+
+const useStyles = makeStyles({
+	...adminLayout,
+	section: {
+		...adminLayout.section,
+		minHeight: 'calc(100vh - 180px)'
+	}
+});
 
 export default function Profile() {
+	const [openUpdate, setOpenUpdate] = useState(false);
+	const classes = useStyles();
+
 	return (
-		<Container>
-			<div className='imgContainer'>
-				<img src='/images/student.png' alt='pro'></img>
-			</div>
-			<div style={{ overflowX: 'auto' }}>
-				<table width='100%'>
-					<tbody>
-						<RowWithTypography title={'Name'} value={'Student 1'} />
-						<RowWithTypography title={'Register NO'} value={'810018104080'} />
-						<RowWithTypography title={'DOB'} value={'08 - 11 - 2000'} />
-						<RowWithTypography title={'Department'} value={'CSE'} />
-						<RowWithTypography title={'Degree'} value={'BE'} />
-						<RowWithTypography title={'Batch'} value={'2018 - 2023'} />
-						<RowWithTypography title={'Campus'} value={'BIT'} />
-						<RowWithTypography title={'Section ID'} value={'12'} />
-						<RowWithTypography title={'Phone'} value={'1234567890'} />
-						<RowWithTypography title={'Email'} value={'student@gmail.com'} />
-					</tbody>
-				</table>
-			</div>
-		</Container>
+		<section className={classes.section}>
+			<Container>
+				<div className='imgContainer'>
+					<img src='/images/student.png' alt='pro'></img>
+				</div>
+				<div style={{ overflowX: 'auto' }}>
+					<table width='100%'>
+						<tbody>
+							<RowWithTypography title={'Name'} value={'Student 1'} />
+							<RowWithTypography title={'Register NO'} value={'810018104080'} />
+							<RowWithTypography title={'DOB'} value={'08 - 11 - 2000'} />
+							<RowWithTypography title={'Department'} value={'CSE'} />
+							<RowWithTypography title={'Degree'} value={'BE'} />
+							<RowWithTypography title={'Batch'} value={'2018 - 2023'} />
+							<RowWithTypography title={'Campus'} value={'BIT'} />
+							<RowWithTypography title={'Section ID'} value={'12'} />
+							<RowWithTypography title={'Phone'} value={'1234567890'} />
+							<RowWithTypography title={'Email'} value={'student@gmail.com'} />
+						</tbody>
+					</table>
+				</div>
+			</Container>
+			<Edit open={openUpdate} setOpen={setOpenUpdate} />
+			<Fab color='secondary' className={classes.float} onClick={() => setOpenUpdate(true)}>
+				<EditIcon />
+			</Fab>
+		</section>
 	);
 }
 
