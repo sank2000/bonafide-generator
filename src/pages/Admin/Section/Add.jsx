5bfbbdb4f11d6cd636cf9f378c642a23bb3897e3
sank2000 <santhoshvelr@yahoo.com>
2021-05-12 11:58:48 +0530

    integrated add section api in admin dashboard ✔

--- src/pages/Admin/Section/Add.jsx
+++ src/pages/Admin/Section/Add.jsx
@@ -1,21 +1,56 @@
+import { useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button, Box } from '@material-ui/core';
+import axios from 'axios';
 
-import { Modal } from 'components';
+import { Modal, ButtonLoader } from 'components';
 import { adminLayout } from 'constants/classes';
+import { handleChange } from 'functions';
+import Snack from 'contexts/Snack';
 
 const useStyles = makeStyles({
 	...adminLayout
 });
 
-export default function Add({ open, setOpen }) {
+export default function Add({ open, setOpen, setLoadData }) {
 	const classes = useStyles();
+	const { setSnack } = useContext(Snack);
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState({
+		name: ''
+	});
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		setLoading(true);
+		try {
+			await axios.post('/admin/section/new', data);
+			setLoading(false);
+			setSnack({
+				open: true,
+				message: 'Section created successfully !',
+				type: 'success'
+			});
+			setTimeout(() => {
+				setOpen(false);
+				setLoadData(old => !old);
+			}, 2000);
+		} catch (error) {
+			setLoading(false);
+			error.handleGlobally && error.handleGlobally();
+		}
+	};
 
 	return (
-		<Modal title={'Add section'} open={open} setOpen={setOpen}>
-			<form>
-				<TextField label='Name' required name='name' />
-				<TextField label='Staff ID' required name='staffID' />
+		<Modal title={'Add section'} open={open} setOpen={setOpen} disableClose={loading}>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					label='Name'
+					required
+					name='name'
+					value={data.name}
+					onChange={handleChange(setData)}
+				/>
 				<Box display='flex' justifyContent='flex-end'>
 					<Button
 						variant='contained'
@@ -27,7 +62,7 @@ export default function Add({ open, setOpen }) {
 						Cancel
 					</Button>
 					<Button variant='contained' color='primary' type='submit'>
-						Add
+						Add {loading && <ButtonLoader />}
 					</Button>
 				</Box>
 			</form>
--- src/pages/Admin/Section/Section.jsx
+++ src/pages/Admin/Section/Section.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Fab, IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import { Table, TableContainer, TableHead, TableRow, Paper, TableBody } from '@material-ui/core';
 import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
+import axios from 'axios';
 
-import { StyledTableCell, StyledTableRow } from 'components';
+import { StyledTableCell, StyledTableRow, PageLoader } from 'components';
 import { adminLayout } from 'constants/classes';
 import Add from './Add';
-import AddStudent from './AddStudent';
 import Edit from './Edit';
 import View from './View';
 
@@ -21,80 +20,82 @@ const useStyles = makeStyles({
 export default function Staff() {
 	const classes = useStyles();
 	const [openAdd, setOpenAdd] = useState(false);
-	const [openAddStudent, setOpenAddStudent] = useState(false);
 	const [openUpdate, setOpenUpdate] = useState(false);
 	const [openView, setOpenView] = useState(false);
-	const data = [
-		{
-			name: 'Section 1',
-			staff: 'Staff 1',
-			noOfStudents: 10
-		},
-		{
-			name: 'Section 2',
-			staff: 'Staff 2',
-			noOfStudents: 15
-		},
-		{
-			name: 'Section 3',
-			staff: 'Staff 3',
-			noOfStudents: 17
+	const [activeDoc, setActiveDoc] = useState({});
+	const [loadData, setLoadData] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState([]);
+
+	const getSection = async () => {
+		setLoading(true);
+		try {
+			const { data: resData } = await axios.get('/admin/section/all');
+			setData(resData.data);
+			setLoading(false);
+		} catch (error) {
+			error.handleGlobally && error.handleGlobally();
 		}
-	];
+	};
+
+	const handleSetActive = (state, value) => {
+		setActiveDoc(value);
+		state(true);
+	};
+
+	useEffect(() => {
+		getSection();
+	}, [loadData]);
 
 	return (
 		<section className={classes.section}>
 			<Typography variant='h4' align='center' className={classes.title}>
 				List
 			</Typography>
-			<TableContainer component={Paper} className={classes.table}>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<StyledTableCell>ID</StyledTableCell>
-							<StyledTableCell>Name</StyledTableCell>
-							<StyledTableCell>Staff</StyledTableCell>
-							<StyledTableCell>No of students</StyledTableCell>
-							<StyledTableCell>Add student</StyledTableCell>
-							<StyledTableCell>View</StyledTableCell>
-							<StyledTableCell>Edit</StyledTableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{data.map((val, ind) => {
-							return (
-								<StyledTableRow key={ind}>
-									<StyledTableCell component='th' scope='row'>
-										{ind + 1}
-									</StyledTableCell>
-									<StyledTableCell>{val.name}</StyledTableCell>
-									<StyledTableCell>{val.staff}</StyledTableCell>
-									<StyledTableCell>{val.noOfStudents}</StyledTableCell>
-									<StyledTableCell>
-										<IconButton onClick={() => setOpenAddStudent(true)}>
-											<AddOutlinedIcon />
-										</IconButton>
-									</StyledTableCell>
-									<StyledTableCell>
-										<IconButton onClick={() => setOpenView(true)}>
-											<VisibilityOutlinedIcon />
-										</IconButton>
-									</StyledTableCell>
-									<StyledTableCell>
-										<IconButton onClick={() => setOpenUpdate(true)}>
-											<EditOutlinedIcon />
-										</IconButton>
-									</StyledTableCell>
-								</StyledTableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<Add open={openAdd} setOpen={setOpenAdd} />
-			<AddStudent open={openAddStudent} setOpen={setOpenAddStudent} />
-			<Edit open={openUpdate} setOpen={setOpenUpdate} />
-			<View open={openView} setOpen={setOpenView} />
+			{!loading && (
+				<TableContainer component={Paper} className={classes.table}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<StyledTableCell>S.no</StyledTableCell>
+								<StyledTableCell>Name</StyledTableCell>
+								<StyledTableCell>No of staffs</StyledTableCell>
+								<StyledTableCell>No of students</StyledTableCell>
+								<StyledTableCell>View</StyledTableCell>
+								<StyledTableCell>Edit</StyledTableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{data.map((val, ind) => {
+								return (
+									<StyledTableRow key={ind}>
+										<StyledTableCell component='th' scope='row'>
+											{ind + 1}
+										</StyledTableCell>
+										<StyledTableCell>{val.name}</StyledTableCell>
+										<StyledTableCell>{val.staffs?.length}</StyledTableCell>
+										<StyledTableCell>{val.students?.length}</StyledTableCell>
+										<StyledTableCell>
+											<IconButton onClick={() => handleSetActive(setOpenView, val)}>
+												<VisibilityOutlinedIcon />
+											</IconButton>
+										</StyledTableCell>
+										<StyledTableCell>
+											<IconButton onClick={() => handleSetActive(setOpenUpdate, val)}>
+												<EditOutlinedIcon />
+											</IconButton>
+										</StyledTableCell>
+									</StyledTableRow>
+								);
+							})}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			)}
+			{loading && <PageLoader />}
+			<Add open={openAdd} setOpen={setOpenAdd} setLoadData={setLoadData} />
+			<Edit open={openUpdate} setOpen={setOpenUpdate} activeDoc={activeDoc} />
+			<View open={openView} setOpen={setOpenView} activeDoc={activeDoc} />
 			<Fab color='secondary' className={classes.float} onClick={() => setOpenAdd(true)}>
 				<AddIcon />
 			</Fab>
