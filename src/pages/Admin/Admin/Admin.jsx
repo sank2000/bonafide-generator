68d3bc8b2cb481b70043dd43ef7436d170fe7f30
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-05-13 19:10:19 +0530

    Admin API Integrated

--- src/pages/Admin/Admin/Admin.jsx
+++ src/pages/Admin/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
 	Typography,
 	Fab,
@@ -10,99 +10,110 @@ import {
 	Paper,
 	TableBody
 } from '@material-ui/core';
+import axios from 'axios';
 import AddIcon from '@material-ui/icons/Add';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 
-import { StyledTableCell, StyledTableRow, Dialog } from 'components';
+import { StyledTableCell, StyledTableRow, Dialog, PageLoader } from 'components';
 import { useAlStyles } from 'constants/classes';
 import Add from './Add';
-import Edit from './Edit';
 
 export default function Admin() {
 	const classes = useAlStyles();
 	const [openAdd, setOpenAdd] = useState(false);
-	const [openUpdate, setOpenUpdate] = useState(false);
 	const [openDelete, setOpenDelete] = useState(false);
-	const data = [
-		{
-			name: 'Admin 1',
-			phone: 9876543210,
-			email: 'admin1@gmail.com'
-		},
-		{
-			name: 'Admin 2',
-			phone: 9876543210,
-			email: 'admin2@gmail.com'
-		},
-		{
-			name: 'Admin 3',
-			phone: 9876543210,
-			email: 'admin3@gmail.com'
+	const [activeDoc, setActiveDoc] = useState({});
+	const [loading, setLoading] = useState(false);
+	const [loadData, setLoadData] = useState(false);
+	const [buttonLoading, setButtonLoading] = useState(false);
+	const [data, setData] = useState([]);
+
+	const getAdmin = async () => {
+		setLoading(true);
+		try {
+			const { data: resData } = await axios.get('/admin/all');
+			setData(resData.data);
+			setLoading(false);
+		} catch (error) {
+			error.handleGlobally && error.handleGlobally();
 		}
-	];
+	};
 
-	const handleDelete = () => {
+	const handleSetActive = (state, value) => {
+		setActiveDoc(value);
+		state(true);
+	};
+
+	const handleDelete = async () => {
+		setButtonLoading(true);
+		try {
+			await axios.delete('/admin/delete', {
+				data: {
+					id: activeDoc._id
+				}
+			});
+			setButtonLoading(false);
+			setLoadData(old => !old);
+		} catch (error) {
+			setButtonLoading(false);
+			error.handleGlobally && error.handleGlobally();
+		}
 		setOpenDelete(false);
 	};
 
-	const handleOpenDelete = id => {
-		setOpenDelete(true);
-	};
-	const handleCloseDelete = () => {
-		setOpenDelete(false);
-	};
+	useEffect(() => {
+		getAdmin();
+	}, [loadData]);
 
 	return (
 		<section className={classes.section}>
 			<Typography variant='h4' align='center' className={classes.title}>
 				List
 			</Typography>
-			<TableContainer component={Paper} className={classes.table}>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<StyledTableCell>ID</StyledTableCell>
-							<StyledTableCell>Name</StyledTableCell>
-							<StyledTableCell>Email</StyledTableCell>
-							<StyledTableCell>Phone</StyledTableCell>
-							<StyledTableCell>Edit</StyledTableCell>
-							<StyledTableCell>Delete</StyledTableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{data.map((val, ind) => {
-							return (
-								<StyledTableRow key={ind}>
-									<StyledTableCell component='th' scope='row'>
-										{ind + 1}
-									</StyledTableCell>
-									<StyledTableCell>{val.name}</StyledTableCell>
-									<StyledTableCell>{val.email}</StyledTableCell>
-									<StyledTableCell>{val.phone}</StyledTableCell>
-									<StyledTableCell>
-										<IconButton>
-											<EditOutlinedIcon onClick={() => setOpenUpdate(true)} />
-										</IconButton>
-									</StyledTableCell>
-									<StyledTableCell>
-										<IconButton onClick={() => handleOpenDelete(ind + 1)}>
-											<DeleteForeverOutlinedIcon />
-										</IconButton>
-									</StyledTableCell>
-								</StyledTableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<Add open={openAdd} setOpen={setOpenAdd} />
-			<Edit open={openUpdate} setOpen={setOpenUpdate} />
+			{!loading && (
+				<TableContainer component={Paper} className={classes.table}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<StyledTableCell>ID</StyledTableCell>
+								<StyledTableCell>Name</StyledTableCell>
+								<StyledTableCell>Email</StyledTableCell>
+								<StyledTableCell>Phone</StyledTableCell>
+								<StyledTableCell>Delete</StyledTableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{data.map((val, ind) => {
+								return (
+									<StyledTableRow key={ind}>
+										<StyledTableCell component='th' scope='row'>
+											{ind + 1}
+										</StyledTableCell>
+										<StyledTableCell>{val.name}</StyledTableCell>
+										<StyledTableCell>{val.email}</StyledTableCell>
+										<StyledTableCell>{val.phoneNumber}</StyledTableCell>
+										<StyledTableCell>
+											<IconButton onClick={() => handleSetActive(setOpenDelete, val)}>
+												<DeleteForeverOutlinedIcon />
+											</IconButton>
+										</StyledTableCell>
+									</StyledTableRow>
+								);
+							})}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			)}
+			{loading && <PageLoader />}
+			<Add open={openAdd} setOpen={setOpenAdd} setLoadData={setLoadData} />
 			<Dialog
-				title='Conform'
+				title='Confirm'
 				description='Are you sure to delete this?'
 				open={openDelete}
-				handleClose={handleCloseDelete}
+				loading={buttonLoading}
+				handleClose={() => {
+					if (!buttonLoading) setOpenDelete(false);
+				}}
 				handleConform={handleDelete}
 			/>
 			<Fab color='secondary' className={classes.float} onClick={() => setOpenAdd(true)}>
