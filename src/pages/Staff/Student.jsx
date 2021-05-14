23e0e1759e0bd5cd7b92dec756a495f1ded23113
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-05-14 15:17:05 +0530

    Integrated view students in staff's section API ✔

--- src/pages/Staff/Student.jsx
+++ src/pages/Staff/Student.jsx
@@ -1,68 +1,70 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Table, TableContainer, TableHead, TableRow, Paper, TableBody } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 
-import { StyledTableCell, StyledTableRow } from 'components';
+import { StyledTableCell, StyledTableRow, PageLoader } from 'components';
 import { useAlStyles } from 'constants/classes';
 
 export default function Student() {
 	const classes = useAlStyles();
-	const data = [
-		{
-			registerNo: '810018104080',
-			name: 'Name',
-			degree: 'BE',
-			department: 'CSE',
-			campus: 'BIT'
-		},
-		{
-			registerNo: '810018104081',
-			name: 'Name',
-			degree: 'BE',
-			department: 'CSE',
-			campus: 'BIT'
-		},
-		{
-			registerNo: '810018104082',
-			name: 'Name',
-			degree: 'BE',
-			department: 'CSE',
-			campus: 'BIT'
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState([]);
+
+	const getStudents = async () => {
+		setLoading(true);
+		try {
+			const { data: resData } = await axios.get('/staff/section/student');
+			setData(resData.data);
+			console.log(resData.data);
+			setLoading(false);
+		} catch (error) {
+			setLoading(false);
+			error.handleGlobally ?? error.handleGlobally();
 		}
-	];
+	};
+
+	useEffect(() => {
+		getStudents();
+	}, []);
 
 	return (
 		<section className={classes.section}>
 			<Typography variant='h4' align='center' className={classes.title}>
-				List
+				{data[0].section.name}
 			</Typography>
-			<TableContainer component={Paper} className={classes.table}>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<StyledTableCell>Register No</StyledTableCell>
-							<StyledTableCell>Name</StyledTableCell>
-							<StyledTableCell>Degree</StyledTableCell>
-							<StyledTableCell>Department</StyledTableCell>
-							<StyledTableCell>Campus</StyledTableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{data.map((val, ind) => {
-							return (
-								<StyledTableRow key={ind}>
-									<StyledTableCell component='th' scope='row'>
-										{val.registerNo}
-									</StyledTableCell>
-									<StyledTableCell>{val.name}</StyledTableCell>
-									<StyledTableCell>{val.degree}</StyledTableCell>
-									<StyledTableCell>{val.department}</StyledTableCell>
-									<StyledTableCell>{val.campus}</StyledTableCell>
-								</StyledTableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-			</TableContainer>
+			{loading ? (
+				<PageLoader />
+			) : (
+				<TableContainer component={Paper} className={classes.table}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<StyledTableCell>Register No</StyledTableCell>
+								<StyledTableCell>Name</StyledTableCell>
+								<StyledTableCell>Degree</StyledTableCell>
+								<StyledTableCell>Department</StyledTableCell>
+								<StyledTableCell>Campus</StyledTableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{data.map((val, ind) => {
+								return (
+									<StyledTableRow key={ind}>
+										<StyledTableCell component='th' scope='row'>
+											{val.registerNumber}
+										</StyledTableCell>
+										<StyledTableCell>{val.name}</StyledTableCell>
+										<StyledTableCell>{val.degree}</StyledTableCell>
+										<StyledTableCell>{val.department}</StyledTableCell>
+										<StyledTableCell>{val.campus}</StyledTableCell>
+									</StyledTableRow>
+								);
+							})}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			)}
 		</section>
 	);
 }
